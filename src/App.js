d3fec34c3072c7688a3848aed4c2123f7941c08f
matthieumelin <matthieumelin62@gmail.com>
2021-01-16 13:15:05 +0100

    Ajout d'une alerte de success Vérification de vote et de sélection d'une réponse Incrémentation du vote

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Question from "./components/Question";
 
 function App() {
-  const [polls, setPolls] = useState(
+  const [questions, setQuestions] = useState(
     [{
       id: 1,
       title: "Question 1",
@@ -29,14 +29,30 @@ function App() {
         { id: 2, name: "Non", votes: 0 },
       ]
     }
-    ]
-  )
+    ])
+
+  const changeQuestion = (newQuestion) => {
+    // création d'une variable constante qui va contenir toutes les questions avec ses propriétés
+    const tempQuestions = [...questions];
+    // findIndex renvoie le premiere index de la liste et on verifie que l'id de la question est égale a la meme que la nouvelle
+    const newQuestionId = tempQuestions.findIndex(
+      question => question.id === newQuestion.id);
+
+    // remplacement de l'ancienne question par la nouvelle
+    tempQuestions[newQuestionId] = newQuestion;
+
+    // mise a jour la liste des questions
+    setQuestions(tempQuestions);
+  }
 
   return (
-    <div className="container">
-      {polls.map(poll => {
-        return <Question key={poll.id} data={poll} setPolls={setPolls} />
-      })}
+    <div className="container mt-5">
+      <h1 className="text-center">Votes</h1>
+      <div className="questions mt-5">
+        {questions.map(question => {
+          return <Question key={question.id} data={question} changeQuestion={changeQuestion} />
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,41 +1,60 @@
 import React, { useState } from 'react'
 
-import Votes from "./Votes";
-
-function Question({data, setPolls}) {
+function Question({ data, changeQuestion }) {
     const [countAnswerVisible, setCountAnswerVisible] = useState(false);
     const [userAnswer, setUserAnswer] = useState(1);
+    const [checked, setChecked] = useState(false);
+    const [voted, setVoted] = useState(false);
 
     const handleVote = (event) => {
-        event.preventDefault();
+        if (!voted && checked) {
+            // désactiver le rafraichissement du formulaire
+            event.preventDefault();
 
-        setCountAnswerVisible(true);
+            // rendre visible le nombre de votes
+            setCountAnswerVisible(true);
+
+            // recuperaton de la liste des questions
+            const newQuestion = { ...data };
+
+            // on récupere les réponses de la question et on incremente +1 aux votes
+            newQuestion.answers[userAnswer - 1].votes++;
+
+            // on remplace la nouvelle question
+            changeQuestion(newQuestion);
+
+            // dire qu'on a voter
+            setVoted(true);
+        }
     }
 
     const handleAnswer = (event) => {
         setUserAnswer(event.target.value);
+        setChecked(event.target.value);
     }
 
     return (
-        <div className="card mb-3">
-            <div className="card-header bg-primary text-white border-primary">
-                {data.title}
-            </div>
-            <div className="card-body">
-                <form>
-                    {data.answers.map(answer => {
-                        const answerId = "q" + data.id + "a" + answer.id;
-                        return <div key={answer.id} className="form-check">
-                            <input type="radio" name={"q" + data.id} id={answerId} value={answer.id} className="form-check-input"
-                            onChange={handleAnswer} />
-                            <label htmlFor={answerId} className="form-check-label">{answer.name} {countAnswerVisible ? answer.votes : null}</label>
-                        </div>
-                    })}
-                </form>
-            </div>
-            <div className="card-footer bg-transparent">
-                <button className="btn btn-primary" onClick={handleVote}>Voter</button>
-                <Votes />
+        <div className="question">
+            { voted ? <div class="alert alert-success" role="alert">Votre vote à était pris en compte.</div> : null }
+            <div className="card mb-3">
+                <div className="card-header bg-primary text-white border-primary">
+                    {data.title}
+                </div>
+                <div className="card-body">
+                    <form>
+                        {data.answers.map(answer => {
+                            const answerId = "q" + data.id + "a" + answer.id;
+                            return <div key={answer.id} className="form-check">
+                                <input type="radio" name={"q" + data.id} id={answerId} value={answer.id} className="form-check-input"
+                                    onChange={handleAnswer} />
+                                <label htmlFor={answerId} className="form-check-label">{answer.name} {countAnswerVisible ? answer.votes : null}</label>
+                            </div>
+                        })}
+                    </form>
+                </div>
+                <div className="card-footer bg-transparent">
+                    <button className="btn btn-primary" onClick={handleVote}>Voter</button>
+                </div>
             </div>
         </div>
     )
